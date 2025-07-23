e836624f41177c291da87abddad82b69642749ef
leutrim-tahiri <160032846+leutrim-tahiri@users.noreply.github.com>
2025-07-23 10:34:15 +0200

    Updated page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -909,7 +909,7 @@ export default function Dashboard() {
                                   )}
                                 </div>
                               </div>
-                            </div>
+                            </div></div>
                           </td>
                           {visibleColumns.company && (
                             <td className="px-6 py-4 whitespace-nowrap">
