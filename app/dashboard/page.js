371f847ba5a8deab78421c15729b16d6369d5901
leutrim-tahiri <160032846+leutrim-tahiri@users.noreply.github.com>
2025-07-29 14:45:45 +0200

    Updated page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -579,7 +579,7 @@ export default function Dashboard() {
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
-                  Active Clients
+                  Converted Clients
                 </CardTitle>
                 <Target className="h-4 w-4 text-muted-foreground" />
               </CardHeader>
